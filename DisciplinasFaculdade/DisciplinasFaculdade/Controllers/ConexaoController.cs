8490f167b38fc871e45ba42f831be98cdc7bc01a
Hercules Karkow <heco_karkow98@hotmail.com>
2018-11-26 01:20:07 -0200

    Gerar Excel das Disciplinas
    Geração do arquivo Excel contendo as Disciplinas de acordo com o urso selecionado.

--- DisciplinasFaculdade/DisciplinasFaculdade/Controllers/ConexaoController.cs
+++ DisciplinasFaculdade/DisciplinasFaculdade/Controllers/ConexaoController.cs
@@ -23,14 +23,27 @@ namespace DisciplinasFaculdade.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(string teste)
+        public ActionResult Index(int? IdCurso)
         {
             try
             {
-                // TODO: Add insert logic here
-                //TesteExcelLibrary();
+                if (ModelState.IsValid)
+                {
+                    var curso = db.Curso.FirstOrDefault(x => x.IdCurso == IdCurso);
+                    List<CursoDisciplina> cursoDisciplina = db.CursoDisciplina.Where(x => x.IdCurso == IdCurso).ToList();
 
-                MergeCells();
+                    List<Disciplina> disciplinas = new List<Disciplina>();
+                    foreach (var item in cursoDisciplina.OrderBy(x => x.Disciplina.Semestre).ToList())
+                    {
+                        disciplinas.Add(item.Disciplina);
+                    }
+                    disciplinas.OrderBy(x => x.Semestre).ToList();
+                    
+                    GerarExcel(curso.Nome, disciplinas);
+                    
+                    return RedirectToAction("Index");
+                }
+
                 return RedirectToAction("Index");
             }
             catch
@@ -67,122 +80,216 @@ namespace DisciplinasFaculdade.Controllers
             }
         }
 
-        public ActionResult MergeCells()
+        public ActionResult GerarExcel(string Curso, List<Disciplina> disciplinas)
         {
+            #region Atributos
+
             Excel.Application oXL;
             Excel._Workbook oWB;
             Excel._Worksheet ws;
             Excel.Range oRng;
             //Start Excel and get Application object.
             oXL = new Excel.Application();
-            oXL.Visible = true;
+            
 
             //Get a new workbook.
             oWB = (Excel._Workbook)(oXL.Workbooks.Add(Missing.Value));
 
             ws = (Excel._Worksheet)oWB.ActiveSheet;
 
+            int linha = 1;
+            
+            #endregion Atributos
 
-            oRng = ws.get_Range("A1", "O1");
+
+
+            #region Construção do Cabeçalho
+
+            #region Curso
+            oRng = ws.get_Range("A"+linha, "O"+linha);
+            oRng.Font.Bold = true;
             oRng.Merge(Missing.Value);
-            oRng.Value2 = "Sistemas de Informação";
+            oRng.Value2 = Curso;
             oRng.Style.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+            oRng.Interior.Color = System.Drawing.Color.Aqua;
+            oRng.Borders.Color = System.Drawing.Color.Black;
+            oRng.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+            linha++;
+            #endregion Curso
+
             
 
-            oRng = ws.get_Range("A2", "O2");
-            oRng.Merge(Missing.Value);
-            oRng.Value2 = "1º Semestre - SIS1";
-            oRng.Style.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
-
-            oRng = ws.get_Range("A3", "B3");
+            #region Dia da Semana
+            oRng = ws.get_Range("A"+linha, "B"+linha);
+            oRng.Font.Bold = true;
             oRng.Merge(Missing.Value);
             oRng.Value2 = "Dia da Semana";
             oRng.Style.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+            oRng.Borders.Color = System.Drawing.Color.Black;
+            oRng.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+            #endregion Dia da Semana
 
-            oRng = ws.get_Range("C3", "F3");
+            #region Disciplina
+            oRng = ws.get_Range("C"+linha, "H"+linha);
+            oRng.Font.Bold = true;
             oRng.Merge(Missing.Value);
             oRng.Value2 = "Disciplina";
             oRng.Style.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+            oRng.Borders.Color = System.Drawing.Color.Black;
+            oRng.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+            #endregion Disciplina
 
-            oRng = ws.get_Range("G3", "I3");
+            #region Professor
+            oRng = ws.get_Range("I"+linha, "M"+linha);
+            oRng.Font.Bold = true;
             oRng.Merge(Missing.Value);
             oRng.Value2 = "Professor";
             oRng.Style.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+            oRng.Borders.Color = System.Drawing.Color.Black;
+            oRng.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+            #endregion Professor
 
-            oRng = ws.get_Range("J3", "K3");
+            #region Sala
+            oRng = ws.get_Range("N"+linha, "O"+linha);
+            oRng.Font.Bold = true;
             oRng.Merge(Missing.Value);
             oRng.Value2 = "Sala";
             oRng.Style.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+            oRng.Borders.Color = System.Drawing.Color.Black;
+            oRng.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+            linha++;
+            #endregion Sala
 
-            return null;
-        }
+            
 
+            #endregion Construção do Cabeçalho
 
+            #region Inserção de Dados
 
+            foreach (var item in disciplinas)
+            {
+                #region Semestre
+                oRng = ws.get_Range("A" + linha, "O" + linha);
+                oRng.Font.Bold = true;
+                oRng.Merge(Missing.Value);
+                oRng.Value2 = String.Format("{0}º Semestre", item.Semestre.ToString());
+                oRng.Style.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+                oRng.Borders.Color = System.Drawing.Color.Black;
+                oRng.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+                linha++;
+                #endregion Semestre 
 
+                #region Dia da Semana
+                oRng = ws.get_Range("A"+linha, "B"+linha);
+                oRng.Merge(Missing.Value);
+                oRng.Value2 = item.DiaDaSemana.ToString();
+                oRng.Style.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+                oRng.Borders.Color = System.Drawing.Color.Black;
+                oRng.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+                #endregion Dia da Semana
 
+                #region Disciplina
+                oRng = ws.get_Range("C" + linha, "H" + linha);
+                oRng.Merge(Missing.Value);
+                oRng.Value2 = item.Nome.ToString(); ;
+                oRng.Style.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+                oRng.Borders.Color = System.Drawing.Color.Black;
+                oRng.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+                #endregion Disciplina
 
-        public ActionResult TesteExcelLibrary()
-        {
-            Workbook workbook = new Workbook();
-            Worksheet worksheet = new Worksheet("First Sheet");
+                #region Professor
+                oRng = ws.get_Range("I" + linha, "M" + linha);
+                oRng.Merge(Missing.Value);
+                oRng.Value2 = item.Professor.Nome.ToString();
+                oRng.Style.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+                oRng.Borders.Color = System.Drawing.Color.Black;
+                oRng.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+                #endregion Professor
 
-            #region Criação das células
+                #region Sala
+                oRng = ws.get_Range("N" + linha, "O" + linha);
+                oRng.Merge(Missing.Value);
+                oRng.Value2 = String.Concat(item.Sala.Nome, " - ", item.Sala.Predio.Descricao).ToString();
+                oRng.Style.HorizontalAlignment = Microsoft.Office.Interop.Excel.XlHAlign.xlHAlignCenter;
+                oRng.Borders.Color = System.Drawing.Color.Black;
+                oRng.Borders.LineStyle = Excel.XlLineStyle.xlContinuous;
+                #endregion Sala
 
-            int row = 0;
-            worksheet.Cells[row, 0] = new Cell("Sistemas de Informação - 2019/01");
-            //worksheet.Cells[row, 0].
-
-
-
-            worksheet.Cells[row, 0] = new Cell("1° Semestre - SIS1");
-            //worksheet.Cells[row, 2] = new Cell((decimal)3.45);
-            //worksheet.Cells[row, 3] = new Cell("Text string");
-            //worksheet.Cells[row, 4] = new Cell("Second string");
-            //worksheet.Cells[row, 5] = new Cell(32764.5, "#,##0.00");
-            //worksheet.Cells[row, 6] = new Cell(DateTime.Now, @"YYYY\-MM\-DD");
-
-            #endregion Criação das células
-
-            #region Configurações de células
-            worksheet.Cells.ColumnWidth[0, 0] = 10000;
-            worksheet.Cells.ColumnWidth[1, 0] = 20000;
-
-            #endregion Configurações de células
-
-
-            //worksheet.Cells[row, 0] = new Cell((short)1);
-            //worksheet.Cells[row, 1] = new Cell(9999999);
-            //worksheet.Cells[row, 2] = new Cell((decimal)3.45);
-            //worksheet.Cells[row, 3] = new Cell("Text string");
-            //worksheet.Cells[row, 4] = new Cell("Second string");
-            //worksheet.Cells[row, 5] = new Cell(32764.5, "#,##0.00");
-            //worksheet.Cells[row, 6] = new Cell(DateTime.Now, @"YYYY\-MM\-DD");
-            //worksheet.Cells.ColumnWidth[0, 0] = 3000;
-            //worksheet.Cells.ColumnWidth[2, 2] = 3000;
-            //worksheet.Cells.ColumnWidth[6, 6] = 8000;
-
-            //Resolve problema: O Excel encontrou conteúdo ilegível / Invalid or corrupt file (unreadable content)
-            while (row < 100)
-	{
-                row++;
-                worksheet.Cells[row, 0] = new Cell(" ");
+                linha++;
             }
 
-            workbook.Worksheets.Add(worksheet);
-
-            MemoryStream stream = new MemoryStream();
-            workbook.Save(stream);
-
-            Response.Clear();
-            Response.ContentType = "application/vnd.ms-excel";
-            Response.AddHeader("content-disposition", string.Format("attachment;filename=Teste_{0}.xls", DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")));
-
-            stream.WriteTo(Response.OutputStream);
-
-            Response.End();
+            #endregion Inserção de Dados
+            oXL.Visible = true;
 
             return null;
         }
+
+
+
+
+
+
+ //       public ActionResult TesteExcelLibrary()
+ //       {
+ //           Workbook workbook = new Workbook();
+ //           Worksheet worksheet = new Worksheet("First Sheet");
+
+ //           #region Criação das células
+
+ //           int row = 0;
+ //           worksheet.Cells[row, 0] = new Cell("Sistemas de Informação - 2019/01");
+ //           //worksheet.Cells[row, 0].
+
+
+
+ //           worksheet.Cells[row, 0] = new Cell("1° Semestre - SIS1");
+ //           //worksheet.Cells[row, 2] = new Cell((decimal)3.45);
+ //           //worksheet.Cells[row, 3] = new Cell("Text string");
+ //           //worksheet.Cells[row, 4] = new Cell("Second string");
+ //           //worksheet.Cells[row, 5] = new Cell(32764.5, "#,##0.00");
+ //           //worksheet.Cells[row, 6] = new Cell(DateTime.Now, @"YYYY\-MM\-DD");
+
+ //           #endregion Criação das células
+
+ //           #region Configurações de células
+ //           worksheet.Cells.ColumnWidth[0, 0] = 10000;
+ //           worksheet.Cells.ColumnWidth[1, 0] = 20000;
+
+ //           #endregion Configurações de células
+
+
+ //           //worksheet.Cells[row, 0] = new Cell((short)1);
+ //           //worksheet.Cells[row, 1] = new Cell(9999999);
+ //           //worksheet.Cells[row, 2] = new Cell((decimal)3.45);
+ //           //worksheet.Cells[row, 3] = new Cell("Text string");
+ //           //worksheet.Cells[row, 4] = new Cell("Second string");
+ //           //worksheet.Cells[row, 5] = new Cell(32764.5, "#,##0.00");
+ //           //worksheet.Cells[row, 6] = new Cell(DateTime.Now, @"YYYY\-MM\-DD");
+ //           //worksheet.Cells.ColumnWidth[0, 0] = 3000;
+ //           //worksheet.Cells.ColumnWidth[2, 2] = 3000;
+ //           //worksheet.Cells.ColumnWidth[6, 6] = 8000;
+
+ //           //Resolve problema: O Excel encontrou conteúdo ilegível / Invalid or corrupt file (unreadable content)
+ //           while (row < 100)
+	//{
+ //               row++;
+ //               worksheet.Cells[row, 0] = new Cell(" ");
+ //           }
+
+ //           workbook.Worksheets.Add(worksheet);
+
+ //           MemoryStream stream = new MemoryStream();
+ //           workbook.Save(stream);
+
+ //           Response.Clear();
+ //           Response.ContentType = "application/vnd.ms-excel";
+ //           Response.AddHeader("content-disposition", string.Format("attachment;filename=Teste_{0}.xls", DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")));
+
+ //           stream.WriteTo(Response.OutputStream);
+
+ //           Response.End();
+
+ //           return null;
+ //       }
     }
 }
